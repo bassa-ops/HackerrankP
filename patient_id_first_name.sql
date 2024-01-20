87374817346ae574c69a9f29042b1a7e4cf017c0
bassa-ops <65884967+bassa-ops@users.noreply.github.com>
2024-01-20 17:52:55 +0530

    Update patient_id_first_name.sql

--- patient_id_first_name.sql
+++ patient_id_first_name.sql
@@ -1,1 +1,3 @@
-Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+/*Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long. */
+select patient_id, first_name from patients
+where first_name like 'S%'and first_name like '%s' and length(first_name)>=6;

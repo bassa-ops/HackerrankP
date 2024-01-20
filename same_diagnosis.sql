04d50bf5761979a99623878ffc917cadceeb606a
bassa-ops <65884967+bassa-ops@users.noreply.github.com>
2024-01-20 19:37:00 +0530

    Create same_diagnosis.sql

---
+++ same_diagnosis.sql
@@ -0,0 +1,5 @@
+/* Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.*/
+
+select patient_id, diagnosis from admissions
+group by patient_id, diagnosis
+having count(*)>1;

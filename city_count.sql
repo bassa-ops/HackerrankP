945b3a3a6e13781e7c9d5460bee8ff062fe4d715
bassa-ops <65884967+bassa-ops@users.noreply.github.com>
2024-01-20 19:55:04 +0530

    Update city_count.sql

--- city_count.sql
+++ city_count.sql
@@ -1,2 +1,6 @@
-Show the city and the total number of patients in the city.
-Order from most to least patients and then by city name ascending.
+/* Show the city and the total number of patients in the city.
+Order from most to least patients and then by city name ascending.*/
+
+select city, count(patient_id) as num_patients from patients
+group by city
+order by count(city) desc, city asc;

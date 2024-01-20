840e6aea5994deddd04f88837740177f235ab7fe
bassa-ops <65884967+bassa-ops@users.noreply.github.com>
2024-01-20 17:40:28 +0530

    Update unique_birth_year.sql

--- unique_birth_year.sql
+++ unique_birth_year.sql
@@ -1,2 +1,3 @@
 /*Show unique birth years from patients and order them by ascending.*/
-Show unique birth years from patients and order them by ascending.
+select distinct year(birth_date) from patients
+order by birth_date;

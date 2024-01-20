c0e65619cd9c4ceb9824b285791b6e6da0c01785
bassa-ops <65884967+bassa-ops@users.noreply.github.com>
2024-01-20 19:25:28 +0530

    Update allergies.sql

--- allergies.sql
+++ allergies.sql
@@ -3,3 +3,9 @@
 select first_name, last_name, allergies from patients
 where allergies='Penicillin' or allergies='Morphine'
 order by allergies, first_name, last_name asc;
+
+/*OR */
+
+select first_name, last_name, allergies from patients
+where allergies in('Penicillin','Morphine')
+order by allergies, first_name, last_name asc;
